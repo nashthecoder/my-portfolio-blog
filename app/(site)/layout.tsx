7dcc812387c2a343709f884c44882971cb5ae000
Naijeria Toweett <naijeria@gmail.com>
2026-03-21 20:30:07 +0300

    Update layout.tsx
    Addded quickjob nav link.

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -12,23 +12,42 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  //get all the pages 
   const pages = await getPages();
+
   return (
     <html lang="en">
       <body className='max-w-3xl mx-auto py-10'>
         <header className="flex items-center justify-between">
-          <Link href='/' className="bg-gradient-to-r from-blue-400 via-white-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Nash The Coder</Link>
+          <Link href='/' className="bg-gradient-to-r from-blue-400 via-white-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">
+            Nash The Coder
+          </Link>
           <div className="flex items-center gap-5 text-sm text-gray-600">
+            {/* Dynamic pages from Sanity */}
             {pages.map((page) => (
-              <Link key={page._id} href={`/${page.slug}`} className="hover:underline">{page.title}</Link>
+              <Link key={page._id} href={`/${page.slug}`} className="hover:underline">
+                {page.title}
+              </Link>
             ))}
+
+            {/* Static link to QuickJobs GH Pages */}
+            <a
+              href="https://nashthecoder.github.io/quickjobs"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              QuickJobs
+            </a>
           </div>
         </header>
+
         <main className='py-20'>{children}</main>
+
         <footer className="fixed bottom-0 w-full bg-white py-3">
           <div className="text-center w-full">
-              <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/nashthecoder" className="hover:underline">NashTheCoder</a>.</span>
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              © 2023 <a href="https://github.com/nashthecoder" className="hover:underline">NashTheCoder</a>.
+            </span>
           </div>
         </footer>
       </body>
